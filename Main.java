da3da9b6b9c89776977f044792ff285d06ec0165
Abigail Limpioso <ALMI@usny5cg1063xc4.usny.capco.com>
2025-01-06 15:41:47 -0500

    save changes for printing lanes and commenting

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello User");
+        System.out.println("Welcome Back");
+
+        System.out.println(3+3);
+        System.out.println(2*10);
+
+        //commenting that this should go last cause no line breaks
+        System.out.print("I will print ");
+        System.out.print("on the same line");
+
+        /* longer comment
+         crabs exist
+         bye
+         */
+
+        
+
+    }
+}
